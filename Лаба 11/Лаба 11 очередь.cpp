be6c3678ef60804295de072fbdc5a168ff4b23d8
ft-nij <ftsybutsynin@mail.ru>
2023-04-25 22:31:16 +0500

    Все 11е лабы

---
+++ Лаба 11/Лаба 11 очередь.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+struct Node {
+    int key;
+    Node* next;
+};
+struct Queue {
+    Node* head;
+    Node* tail;
+};
+// Создание очереди
+Queue* createQueue() {
+    Queue* q = new Queue;
+    q->head = nullptr;
+    q->tail = nullptr;
+    return q;
+}
+// Добавление элемента в очередь
+void enqueue(Queue* q, int key) {
+    Node* newNode = new Node;
+    newNode->key = key;
+    newNode->next = nullptr;
+    if (q->tail == nullptr) {
+        q->head = newNode;
+        q->tail = newNode;
+    }
+    else {
+        q->tail->next = newNode;
+        q->tail = newNode;
+    }
+}
+// Удаление элемента из очереди
+void dequeue(Queue* q) {
+    if (q->head == nullptr) {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    Node* temp = q->head;
+    q->head = q->head->next;
+    delete temp;
+    if (q->head == nullptr) {
+        q->tail = nullptr;
+    }
+}
+// Печать очереди
+void printQueue(Queue* q) {
+    Node* currentNode = q->head;
+    while (currentNode != nullptr) {
+        cout << currentNode->key << " ";
+        currentNode = currentNode->next;
+    }
+    cout << endl;
+}
+// Запись очереди в файл
+void saveQueueToFile(Queue* q, string filename) {
+    ofstream file;
+    file.open(filename);
+    Node* currentNode = q->head;
+    while (currentNode != nullptr) {
+        file << currentNode->key << endl;
+        currentNode = currentNode->next;
+    }
+    file.close();
+}
+// Восстановление очереди из файла
+Queue* loadQueueFromFile(string filename) {
+    Queue* q = createQueue();
+    ifstream file;
+    file.open(filename);
+    int key;
+    while (file >> key) {
+        enqueue(q, key);
+    }
+    file.close();
+    return q;
+}
+// Уничтожение очереди
+void destroyQueue(Queue* q) {
+    while (q->head != nullptr) {
+        dequeue(q);
+    }
+    delete q;
+}
+int main() {
+    Queue* q = createQueue();
+    setlocale(0, "rus");
+    int k;
+    int val;
+    cout << "Введите количество элементов: ";
+    cin >> k;
+    for (int i = 1; i <= k; i++) {
+        cout << i << ". ";
+        cin >> val;
+        enqueue(q, val);
+    }
+    cout << "Очередь: ";
+    printQueue(q);
+    dequeue(q);
+    cout << "Очередь после удаления: ";
+    printQueue(q);
+    cout << "Очередь после добавления элемента: ";
+    enqueue(q, 4);
+    printQueue(q);
+    cout << "Очередь успешно записана в файл!" << endl;
+    cout << "Очередь успешно удалена!" << endl;
+    saveQueueToFile(q, "queue.txt");
+    destroyQueue(q);
+    Queue* loadedQueue = loadQueueFromFile("queue.txt");
+    cout << "Очередь после восстановления из файла: ";
+    printQueue(loadedQueue);
+    destroyQueue(loadedQueue);
+    return 0;
+}
---
+++ Лаба 11/Лаба 11 стек.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+struct Node // структура узла стека
+{
+    int key; // ключевое поле
+    Node* next; // указатель на следующий элемент
+};
+void createStack(Node*& top) // создание пустого стека
+{
+    top = NULL;
+}
+void push(Node*& top, int key) // добавление элемента в стек
+{
+    Node* p = new Node; // создаем новый элемент
+    p->next = top;
+    p->key = key; // присваиваем ему значение ключа
+     // новый элемент становится первым
+    top = p; // вершина стека указывает на новый элемент
+}
+void pop(Node*& top) // удаление верхнего элемента из стека
+{
+    if (top == NULL) // если стек пустой
+    {
+        cout << "Стек пустой!" << endl;
+        return;
+    }
+    Node* p = top; // сохраняем вершину стека
+    top = top->next; // вершина стека указывает на следующий элемент
+    delete p; // удаляем сохраненный элемент
+}
+void printStack(Node* top) // печать стека
+{
+    if (top == NULL) // если стек пустой
+    {
+        cout << "Стек пустой!" << endl;
+        return;
+    }
+    while (top != NULL) // пока не достигнут конец стека
+    {
+        cout << top->key << " "; // печатаем значение ключа
+        top = top->next; // переходим к следующему элементу
+    }
+    cout << endl;
+}
+void writeStackToFile(Node* top, string fileName) // запись стека в файл
+{
+    ofstream file(fileName); // создаем файл для записи
+    if (file.is_open()) // если файл открыт
+    {
+        while (top != NULL) // пока не достигнут конец стека
+        {
+            file << top->key << " "; // записываем значение ключа
+            top = top->next; // переходим к следующему элементу
+        }
+        file.close(); // закрываем файл
+    }
+    else // если файл не открыт
+    {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+}
+void destroyStack(Node*& top) // уничтожение стека
+{
+    while (top != NULL) // пока не достигнут конец стека
+    {
+        Node* p = top; // сохраняем вершину стека
+        top = top->next; // вершина стека указывает на следующий элемент
+        delete p; // удаляем сохраненный элемент
+    }
+}
+void restoreStackFromFile(Node*& top, string fileName) // восстановление стека из файла
+{
+    destroyStack(top); // уничтожаем существующий стек
+    ifstream file(fileName); // открываем файл для чтения
+    if (file.is_open()) // если файл открыт
+    {
+        int key;
+        while (file >> key) // пока не достигнут конец файла
+        {
+            push(top, key); // добавляем элемент в стек
+        }
+        file.close(); // закрываем файл
+    }
+    else // если файл не открыт
+    {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // подключаем русский язык
+    Node* top; // указатель на вершину стека
+    int n, k;
+    createStack(top); // создаем пустой стек
+    cout << "Введите количество элементов в стеке: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) // заполняем стек
+    {
+        int key;
+        cout << "Введите элемент " << i + 1 << ": ";
+        cin >> key;
+        push(top, key); // добавляем элемент в стек
+    }
+    cout << "Стек: ";
+    printStack(top); // печатаем стек
+    cout << "Введите элемент, который нужно добавить: ";
+    cin >> k;
+    push(top, k); // добавляем элемент с заданным номером
+    cout << "Стек после добавления элемента: ";
+    printStack(top); // печатаем стек
+    cout << "Введите количество элементов, которые нужно удалить: ";
+    cin >> k;
+    for (int i = 0; i < k; i++) // удаляем элементы из конца стека
+    {
+        pop(top);
+    }
+    cout << "Стек после удаления элементов: ";
+    printStack(top); // печатаем стек
+    writeStackToFile(top, "stack.txt"); // записываем стек в файл
+    destroyStack(top); // уничтожаем стек
+    restoreStackFromFile(top, "stack.txt"); // восстанавливаем стек из файла
+    cout << "Стек после восстановления из файла: ";
+    printStack(top); // печатаем стек
+    destroyStack(top); // уничтожаем стек
+    return 0;
+}
